7c27d6daed5838c6fea1ecc1189531077f5c9fe8
Gragaraa <gragaranew@gmail.com>
2025-10-03 20:42:44 +0300

    lesson 5
    login and clear

--- Main/Papp/urls.py
+++ Main/Papp/urls.py
@@ -20,6 +20,8 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.page, name="index"),
-    path("reg",views.pagereg),
+    path("reg",views.pagereg,name="register"),
     path("users/", views.show_users, name="users"),
+    path("login/", views.user_login, name="login"),
+    path("logout/", views.user_logout, name="logout"),
 ]
--- Main/Papp/views.py
+++ Main/Papp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,redirect
 from . import forms
 from . import models
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate, logout
 from .models import User
+from django.contrib.auth.forms import AuthenticationForm
 def page(request):
     list_event = models.Event.objects.all()
     data={
@@ -14,16 +15,16 @@ def page(request):
 def pagereg(request):
 
 
-    # form=forms.MainInfoForm()
-    # if request.method == "POST":
-    #     form= forms.MainInfoForm(request.POST)
-    #     if form.is_valid():
-    #         user=form.save()
-    #         print("✅ Пользователь сохранён:", user)
-    #         login(request,user)
-    #         return redirect("index")
+    form=forms.MainInfoForm()
+    if request.method == "POST":
+        form= forms.MainInfoForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            print("✅ Пользователь сохранён:", user)
+            login(request,user)
+            return redirect("index")
 
-    form=forms.EventForm()
+
     data={
         "form":form
     }
@@ -35,3 +36,24 @@ def show_users(request):
         'users': users
     }
     return render(request, 'Papp/users.html', data)
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+
+                next_url = request.GET.get('next', 'index')
+                return redirect(next_url)
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'Papp/login.html', {'form': form})
+def user_logout(request):
+    logout(request)
+    return redirect('index')
